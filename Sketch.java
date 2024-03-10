f3f9bdc7ecb069b9b6e830a03cdf6efca9f3845b
juanjoseph123 <juan.joseph25@ycdsbk12.ca>
2024-03-09 23:34:51 -0500

    task 3

--- Sketch.java
+++ Sketch.java
@@ -17,20 +17,51 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
+    int hr = hour();
+    int min = minute();
+
+    fill(0);
+    textSize(30);
+    text(hr,200,300);
+    text(min,200,225);
+    
+    
+    
+    
+    //float ellipseX = random(0,200);
+    //float ellipseY = random(0,200);
+    float ellipseWidth = random(10,50);
+    float ellipseHeight = random(10,50);
+    //ellipse(ellipseX, ellipseY, ellipseHeight, ellipseWidth);
+  
+  float randomX = random(0, 400);
+  float randomY = random(0, 400);
+  if (randomX < 200 && randomY<200) {
+    ellipse(randomX,randomY,ellipseWidth,ellipseHeight);
+  }
+  else if (randomX > 200 && randomY < 200) {
+    ellipse(ellipseWidth, ellipseHeight, randomX, randomY);
+  }
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+  //(public void draw() {
+fill(225);
+  rect(random(50, 200), random(50, 200), random(50, 200), random(50, 200));
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
-}
+fill(255, 0, 0);
+triangle(random(100, 300), (random(100, 300)),(random(100, 300)), (random(100, 300)),(random(100, 300)), random(100, 300));
+
+fill(153, 102, 0);
+rect(random(40, 280), random(40, 280), random(40, 280), random(40, 280));
+
+fill(51, 153, 255);
+rect(random(30,250), random(30,250), random(30,250), random(30,250));
+
+
+
+
+
+}}
